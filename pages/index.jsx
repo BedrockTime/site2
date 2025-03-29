47c7ca75212ef02593a0ce23881699c84dd20843
BedrockTime <bedrocktime12345@yandex.ru>
2025-03-29 19:28:40 +0500

    test3

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,11 +1,19 @@
 const App = () => {
   return(
     <div>
-      <div>
-        <div className="name">
-          <p>Не</p>
-          <h1>DNS</h1>
-          <p>asdawd</p>
+      <div className="header">
+        <div className="name_div">
+          <div className="name">
+            <p>Не</p>
+            <h1>DNS</h1>
+          </div>
+          <div className="name_catalog">
+            <p>Каталог</p>
+          </div>
+        </div>
+        <div className="panel">
+          <p>Корзина</p>
+          <p>Войти</p>
         </div>
       </div>
     </div>
