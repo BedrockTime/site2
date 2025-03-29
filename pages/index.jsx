76cd82ad135fef04cd8c505bf1cced9d14418776
unknown <bedrocktime12345@yandex.ru>
2025-03-29 18:50:05 +0500

    test

---
+++ pages/index.jsx
@@ -0,0 +1,14 @@
+const App = () => {
+  return(
+    <div>
+      <div>
+        <div className="name">
+          <p>Не</p>
+          <h1>DNS</h1>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default App
